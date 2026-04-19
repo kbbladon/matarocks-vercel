7c046a3caaaeb7f6b92d28ec65d6a461f723adbb
MataRocksbze <matarocksbzresort@outlook.com>
2026-04-18 22:45:57 -0600

    fix: simplify preview route, rely on previewSecret only

--- src/app/(frontend)/next/preview/route.ts
+++ src/app/(frontend)/next/preview/route.ts
@@ -1,15 +1,9 @@
-import type { CollectionSlug, PayloadRequest } from 'payload'
-import { getPayload } from 'payload'
-
+import type { CollectionSlug } from 'payload'
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
-import configPromise from '@payload-config'
-
 export async function GET(req: NextRequest): Promise<Response> {
-  const payload = await getPayload({ config: configPromise })
-
   const { searchParams } = new URL(req.url)
 
   const path = searchParams.get('path')
@@ -26,30 +20,10 @@ export async function GET(req: NextRequest): Promise<Response> {
   }
 
   if (!path.startsWith('/')) {
-    return new Response('This endpoint can only be used for relative previews', { status: 500 })
-  }
-
-  let user
-
-  try {
-    user = await payload.auth({
-      req: req as unknown as PayloadRequest,
-      headers: req.headers,
-    })
-  } catch (error) {
-    payload.logger.error({ err: error }, 'Error verifying token for live preview')
-    return new Response('You are not allowed to preview this page', { status: 403 })
+    return new Response('Invalid path', { status: 500 })
   }
 
   const draft = await draftMode()
-
-  if (!user) {
-    draft.disable()
-    return new Response('You are not allowed to preview this page', { status: 403 })
-  }
-
-  // You can add additional checks here to see if the user is allowed to preview this page
-
   draft.enable()
 
   redirect(path)
